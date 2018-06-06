2da3aca7eaafa57e01b7ba650681cdd539fe2ec5
kevinl95 <kevin.loeffler@colorado.edu>
2018-06-06 17:49:13 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# roller-coaster-test-meter
+# Roller Coaster Test Meter
+
+## Roller Coaster Tycoon ride ratings IRL
+
+Are you obsessed with roller coasters? Do you love building the rides of your dreams in Roller Coaster Tycoon? What if you could see how your favorite rides rate using the intensity, excitement, and nausea formulas used in the original Roller Coaster Tycoon? This Ionic and Cordova app for Android and iOS lets you use the accelerometer in your phone to extract lateral and vertical G's from your ride, plot them, and then convert these into a score you can share to social media and with your friends!
+
+
+# Download
+
+**Links to app stores pending!**
+
+# Setting up the Project
+
+The Roller Coaster Test Meter uses Ionic v1. To set up the latest version of the Ionic CLI type the following:
+
+    npm install -g ionic@latest
+ Install the Cordova Device Motion plugin so that we can use the accelerometer:
+
+   ```
+ionic cordova plugin add cordova-plugin-device-motion
+npm install --save @ionic-native/device-motion
+```
+We also need the Cordova Social Message plugin. Using the Cordova CLI:
+
+   ```
+cordova plugin add cordova-plugin-social-message
+```
+
+[Now follow the Ionic v1 setup steps here to get this project set up](https://docs.usecreator.com/docs/zip-export-an-ionic-project). It will be easier if you download the ZIP archive from Github.
---
+++ js/controllers.js
@@ -0,0 +1,174 @@
+angular.module('app.controllers', [])
+  
+.controller('rollerCoasterTestMeterCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+    var time = []
+    var lateral = []
+    var vertical = []
+    var ctx = document.getElementById("latChart");
+    ctx.style.backgroundColor = 'rgba(39,81,19,0.9)';
+    var vctx = document.getElementById("vertChart");
+    vctx.style.backgroundColor = 'rgba(39,81,19,0.9)';
+    var latChart = new Chart(ctx, {
+              type: 'line',
+              data: {
+                labels: time,
+                lineColor: "white",
+                datasets: [
+                  { 
+                    data: lateral
+                  }
+                ]
+              },
+              options: {
+                  title: {
+                        display: true,
+                        text: 'Lateral Gs',
+                        fontColor: 'white',
+                  },
+                  legend: {
+                        display: false,
+                  },
+                  scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true,
+                                fontColor: 'white'
+                            },
+                        }],
+                      xAxes: [{
+                            ticks: {
+                                fontColor: 'white'
+                            },
+                        }]
+                  } 
+              },
+            });
+    var vertChart = new Chart(vctx, {
+              type: 'line',
+              data: {
+                labels: time,
+                lineColor: "white",
+                datasets: [
+                  { 
+                    data: vertical
+                  }
+                ]
+              },
+              options: {
+                  title: {
+                        display: true,
+                        text: 'Vertical Gs',
+                        fontColor: 'white',
+                  },
+                  legend: {
+                        display: false,
+                  },
+                  scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true,
+                                fontColor: 'white'
+                            },
+                        }],
+                      xAxes: [{
+                            ticks: {
+                                fontColor: 'white'
+                            },
+                        }]
+                  }
+              },
+            });
+    var running = false;
+    $scope.watchAcceleration = function(){
+        running = true;
+        var accelerometerOptions = {
+              frequency: 1000
+           }
+           
+            var watchID = navigator.accelerometer.watchAcceleration(
+                accelerometerSuccess, accelerometerError, accelerometerOptions);
+
+           function accelerometerSuccess(acceleration) {
+               if(running){
+                  var t = new Date(acceleration.timestamp);
+                  time.push(t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds());
+                  lateral.push(acceleration.z/9.8);
+                  vertical.push(acceleration.x/9.8);
+                  latChart.update();
+                  vertChart.update();
+               }
+           };
+        
+           function accelerometerError() {
+              alert('onError!');
+           };
+    };
+    
+       
+    $scope.endTest = function(){
+       function rating(score){
+           if (score<2.56){
+              rate = 'Low';
+           } else if (score >= 2.56 && score <= 5.12){
+               rate = 'Medium';
+           } else if (score >= 5.13 && score <= 7.68){
+               rate = 'High';
+           } else if (score >= 7.69 && score <= 10.24){
+               rate = 'Very High';
+           } else if (score >= 10.25 && score <= 12.79){
+               rate = 'Extreme';
+           } else if (score >= 12.8){
+               rate = 'Ultra-Extreme';
+           }
+           return rate;
+       }
+       running = false;
+       max_positive_g = Math.max(...vertical);
+       max_negative_g = Math.min(...vertical);
+       max_lateral_g = Math.max(...lateral);
+       excitement=0.08*max_positive_g-0.24*Math.max(-2.5,max_negative_g)+0.4*Math.min(1.5,max_lateral_g);
+       // Empirically derived multiplier
+       excitement = excitement * 5.52884615385;
+       intensity=0.8*max_positive_g+0.8*(1-max_negative_g)+max_lateral_g;
+       // Empirically derived multiplier
+       intensity = intensity * 0.82392026578;
+       nausea=0.26*max_positive_g+0.22*(1-max_negative_g)+0.33*max_lateral_g;
+       // Empirically derived multiplier
+       nausea = nausea * 1.18965517241;
+       exc_rating = rating(excitement);
+       int_rating = rating(intensity);
+       nau_rating = rating(nausea);
+       document.getElementById("exc").innerHTML = "Excitement: " + excitement.toFmgixed(2) + ' (' + exc_rating + ')';
+       document.getElementById("int").innerHTML = "Intensity: " + intensity.toFixed(2) + ' (' + int_rating + ')';
+       document.getElementById("nau").innerHTML = "Nausea: " + nausea.toFixed(2) + ' (' + nau_rating + ')';
+    };
+    
+    $scope.share = function(){
+        var coaster_name = $scope.data.name
+        if (coaster_name.length === 0){
+            coaster_name = 'this roller coaster';
+        }
+        var message = {
+            text: 'Roller Coaster Test Meter: If ' + coaster_name + ' were built in Roller Coaster Tycoon, it would have the following stats:' + '\n' + document.getElementById("exc").innerHTML + '\n' + document.getElementById("int").innerHTML + '\n' + document.getElementById("nau").innerHTML,
+        };
+        window.socialmessage.send(message);
+    }
+    
+    $scope.clear = function(){
+        time.length = 0;
+        lateral.length = 0;
+        vertical.length = 0;
+        latChart.update();
+        vertChart.update();
+        $scaope.data.name = '';
+    };
+    
+    $scope.data = {
+        'name' : ''
+    }
+
+}])
+ 
